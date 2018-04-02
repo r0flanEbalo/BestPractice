46d4b93bbc9fa145c5d336935ba8535dc89aa5c7
Artem <semenov.artem199607@yandex.ru>
2018-04-02 22:15:21 +0300

    02.04.2018 Динамическая инициализация.

---
+++ src/DynInit.java
@@ -0,0 +1,10 @@
+//В этом примере демонстрируется динамическая инициализация переменных
+public class DynInit {
+    public static void main(String args[]){
+        double a = 3.0, b = 4.0;
+
+        //динамическая инициализация переменной с
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("Гипотенуза равна " + c);
+    }
+}
